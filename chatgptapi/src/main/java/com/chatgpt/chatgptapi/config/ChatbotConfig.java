3d01367fabd06103f69874484d6f8436f3920b5e
Balloon208 <gnswldms2@gmail.com>
2024-03-13 19:35:42 +0900

    Add .gitignore and hide yml file

--- chatgptapi/src/main/java/com/chatgpt/chatgptapi/config/ChatbotConfig.java
+++ chatgptapi/src/main/java/com/chatgpt/chatgptapi/config/ChatbotConfig.java
@@ -1,9 +1,18 @@
 package com.chatgpt.chatgptapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class ChatbotConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "API_KEY";
+
+    public static String API_KEY;
+
+    @Value("${chatbot.api_key}")
+    public void setApiKey(String apiKey) {
+        API_KEY = apiKey;
+    }
+
     public static final String MODEL = "gpt-3.5-turbo";
     public static final Integer MAX_TOKEN = 3000;
     public static final Double TEMPERATURE = 0.0;
